801d73d62f1a884d2ecd1a17598e3adc411870d7
Alyaa-Abdelmoniem <aliaelshetry1@gmail.com>
2026-07-09 14:44:53 +0300

    Add files via upload

---
+++ Task1__iti.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Enter operator: ";
+    cin >> op;
+
+    if (op == '+')
+    {
+        cout << num1 + num2;
+    }
+    else if (op == '-')
+    {
+        cout << num1 - num2;
+    }
+    else if (op == '*')
+    {
+        cout << num1 * num2;
+    }
+    else if (op == '/')
+    {
+        cout << num1 / num2;
+    }
+    else if (op == '%')
+    {
+        cout << num1 % num2;
+    }
+
+    return 0;
+}
